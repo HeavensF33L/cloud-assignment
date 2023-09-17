3da0f176bd4d16cd8ad22317c12c84d533149b5f
Wei Xin <78343614+HeavensF33L@users.noreply.github.com>
2023-09-17 21:09:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cgzss1uvc4e8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "kungweixin-employee"
+customregion = "us-east-1"
